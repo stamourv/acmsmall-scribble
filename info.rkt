67973ae2f8aa4b2aa789fdbcbfcdfbf2cd9afc87
Leif Andersen <leif@leifandersen.net>
2016-02-19 14:24:01 -0500

    Fix dependencies.

--- info.rkt
+++ info.rkt
@@ -3,6 +3,7 @@
 (define collection "scribble")
 
 (define deps '("base" "scribble-lib" "at-exp-lib"))
+(define build-deps '("racket-doc" "scribble-doc"))
 
 (define pkg-desc "Port of the acmsmall style to Scribble")
 (define pkg-authors '(stamourv))
